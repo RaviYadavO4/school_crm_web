68673a785c82434451ff5f6daefa19c64d9ea0f1
Ravi Yadav <ravi04all@gmail.com>
2025-10-29 00:59:14 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "328561249"
+    serviceWorkerVersion: "4009941810"
   }
 });
